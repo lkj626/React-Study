d38dfd2dec4317a42e917611285d762646b93c2c
이강진 <2020090264@hanyang.ac.kr>
2022-08-08 23:02:37 +0900

    redux-thunk

--- redux-middleware/src/middlewares/myLoggers.js
+++ redux-middleware/src/middlewares/myLoggers.js
@@ -2,6 +2,7 @@ const myLogger = store => next => action => {
     console.log(action);
     console.log('\tPrev:', store.getState());
     const result = next(action);
+    
     console.log('\tNext:', store.getState());
     return result;
 };
