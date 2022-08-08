83d2346bdeac68aacc7a067d838fc7b096195f09
이강진 <hwi2337@naver.com>
2022-08-08 23:11:18 +0900

    config email

--- redux-middleware/src/containers/CounterContainer.js
+++ redux-middleware/src/containers/CounterContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Counter from '../components/Counter';
 import {useSelector , useDispatch } from 'react-redux';
-import { decrease, decreaseAsync, increase, increaseAsync } from '../modules/counter';
+import { decreaseAsync, increaseAsync } from '../modules/counter';
 
 function CounterContainer() {
     const number = useSelector(state => state.counter);
